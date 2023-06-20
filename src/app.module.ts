a2b632311002602deef554f26078237d5ad5af15
gm50x <getuliomagela@outlook.com>
2023-06-20 20:00:33 -0300

    removes controllers and app service as they should be in different layer and should be added later

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TracingModule } from './config';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), TracingModule.forHTTP()],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
